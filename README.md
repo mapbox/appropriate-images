bec6b82e887c4d395d23b0dbab38a90c8f8e5b2c
David Clark <dclark@mapbox.com>
2017-06-12 15:17:56 -0700

    A start

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # appropriate-images
+
 Get appropriately sized images into your website.
+
+🚧🚧 **EXPERIMENTAL! WORK IN PROGRESS!** 🚧🚧
---
+++ test/generate.test.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const fs = require('fs');
+const pify = require('pify');
+const del = require('del');
+const path = require('path');
+const generate = require('../lib/generate').generate;
+
+describe('generate', () => {
+  const inputDirectory = path.join(__dirname, './fixtures');
+  const outputDirectory = path.join(__dirname, './output');
+
+  afterEach(() => {
+    return del(path.join(outputDirectory, '*.*'));
+  });
+
+  test('works', () => {
+    const imageConfig = {
+      bear: {
+        basename: 'bear.png',
+        sizes: [{ width: 300 }, { width: 600 }]
+      },
+      montaraz: {
+        basename: 'montaraz.jpg',
+        sizes: [
+          { width: 300, height: 500 },
+          { width: 1200, crop: 'north' },
+          { width: 200, height: 200, crop: 'southeast' },
+          { width: 210, height: 210, crop: 'northwest' }
+        ]
+      },
+      osprey: {
+        basename: 'osprey.jpg',
+        sizes: [{ width: 600 }, { width: 300, height: 300 }]
+      },
+      walrus: {
+        basename: 'walrus.png',
+        sizes: [{ width: 400 }]
+      }
+    };
+
+    const options = { inputDirectory, outputDirectory };
+
+    return generate(imageConfig, options)
+      .then(() => {
+        return pify(fs.readdir)(outputDirectory);
+      })
+      .then(outputFiles => {
+        expect(outputFiles).toEqual([
+          'bear-300.png',
+          'bear-300.webp',
+          'bear-600.png',
+          'bear-600.webp',
+          'montaraz-1200.jpg',
+          'montaraz-1200.webp',
+          'montaraz-200x200.jpg',
+          'montaraz-200x200.webp',
+          'montaraz-210x210.jpg',
+          'montaraz-210x210.webp',
+          'montaraz-300x500.jpg',
+          'montaraz-300x500.webp',
+          'osprey-300x300.jpg',
+          'osprey-300x300.webp',
+          'osprey-600.jpg',
+          'osprey-600.webp',
+          'walrus-400.png',
+          'walrus-400.webp'
+        ]);
+      });
+  });
+});
