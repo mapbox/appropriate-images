219627e0c220ed2b449ac0d08fa1d1faea1bacb4
Andrew Harvey <andrew@alantgeo.com.au>
2020-05-24 12:13:42 +1000

    upgrade to latest sharp

--- test/generate.test.js
+++ test/generate.test.js
@@ -26,9 +26,9 @@ describe('generate', () => {
         basename: 'montaraz.jpg',
         sizes: [
           { width: 300, height: 500 },
-          { width: 1200, crop: 'north' },
-          { width: 200, height: 200, crop: 'southeast' },
-          { width: 210, height: 210, crop: 'northwest' }
+          { width: 1200, options: { position: 'north' } },
+          { width: 200, height: 200, options: { position: 'southeast' } },
+          { width: 210, height: 210, options: { position: 'northwest' } }
         ]
       },
       osprey: {
@@ -79,9 +79,9 @@ describe('generate', () => {
         basename: 'montaraz.jpg',
         sizes: [
           { width: 300, height: 500 },
-          { width: 1200, crop: 'north' },
-          { width: 200, height: 200, crop: 'southeast' },
-          { width: 210, height: 210, crop: 'northwest' }
+          { width: 1200, options: { position: 'north' } },
+          { width: 200, height: 200, options: { position: 'southeast' } },
+          { width: 210, height: 210, options: { position: 'northwest' } }
         ]
       },
       osprey: {
